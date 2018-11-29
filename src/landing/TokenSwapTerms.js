3f65bfe56c46ea53b236e6afaa0a8ddae94fbf6e
fpsbernardo <fpsbernardo@gmail.com>
2018-11-29 11:16:55 +0200

    Create token swap terms page

---
+++ src/landing/TokenSwapTerms.js
@@ -0,0 +1,15 @@
+// @flow
+import React from 'react';
+import { default as Page, Body } from './Page';
+
+import { Heading } from './ui';
+
+export const Legal = () => (
+  <Page>
+    <Heading>Token Swap Terms and Conditions</Heading>
+    <Body>
+    </Body>
+  </Page>
+);
+
+export default Legal;
